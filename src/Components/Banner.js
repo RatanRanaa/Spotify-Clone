1b00688bc4c8270e915bcdd3dbfce2f5ecdab7c1
Ratan Rana <ratanrana206@gmail.com>
2023-02-07 11:04:49 +0530

    Banner.js

---
+++ src/Components/Banner.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FaEllipsisH, FaHeadphones, FaCheck } from 'react-icons/fa'
+
+import Artist from '../img/artist.jpg';
+import Check from '../img/check.png';
+
+const Banner = () => {
+  return (
+    <>
+      <div className="banner">
+        <img className='bannerImg' src={Artist} alt="logo" />
+
+        <div className="content">
+          <div className="breadCrump">
+            <p>Home <span>/Popular Artist</span></p>
+
+            <i> <FaEllipsisH/> </i>
+          </div>
+            <div className="artist">
+              <div className="left">
+                <div className="name">
+                  <h2>A-Ha</h2>
+                  <img src={Check} alt="" />
+                </div>
+                <p><i><FaHeadphones/></i> 11,184,1811 <span>Monthly listeners</span></p>
+              </div>
+              <div className="right">
+                <a href="/">Play</a>
+                <a href="/"><i><FaCheck/></i>Following</a>
+              </div>
+            </div>
+        </div>
+
+        <div className="bottomLayer"></div>
+      </div>
+    </>
+  )
+}
+
+export {Banner}
--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../Styles/MainContainer.css'
+import { Banner } from './Banner'
 
 const MainContainer = () => {
   return (
     <>
       <div className="mainContainer">
-        
+        <Banner/>
       </div> 
     </>
   )
