21da85783ca41b501ba562d2118ed2a31d42d37f
Ratan Rana <ratanrana206@gmail.com>
2023-02-10 11:07:04 +0530

    RightMenu

--- src/Components/RightMenu.js
+++ src/Components/RightMenu.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import Profile from '../img/profile.jpg'
+import { FaBell, FaCogs, FaCrown, FaRegHeart, FaSun } from 'react-icons/fa'
 import '../Styles/RightMenu.css'
 
 const RightMenu = () => {
   return (
     <>
       <div className="rightMenu">
-          
+          <div className="goPro">
+            <i>
+              <FaCrown />
+              <p>
+                Go <span>Pro</span>
+              </p>
+            </i>
+              <i>
+                <FaBell/>
+              </i>
+            
+            <i>
+              <FaRegHeart/>
+            </i>
+
+          </div>
+          <div className="profile">
+            <i><FaSun/></i>
+            <i><FaCogs/></i>
+            <div className="profileIMage">
+              <img src={Profile} alt="" />
+            </div>
+          </div>
       </div> 
     </>
   )
